48d7522c737a2408d2aba950187e255a426410d9
EpicalxD <morschl.alex@gmail.com>
2022-08-20 15:23:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+const scoreHome = document.getElementById("home");
+const scoreGuest = document.getElementById("guest");
+const period = document.getElementById("period");
+const playerNumber = document.getElementById("player");
+const foultsNumber = document.getElementById("foults");
+let homeScore = 0;
+let guestScore = 0;
+let periodNumber = 0;
+let playerNr = 0;
+let foultNr = 0;
+
+// Add one
+
+function addOneHome() {
+  homeScore++;
+  scoreHome.textContent = homeScore;
+}
+function addOneGuest() {
+  guestScore++;
+  scoreGuest.textContent = guestScore;
+}
+
+// Add two
+
+function addTwoHome() {
+  homeScore = homeScore + 2;
+  scoreHome.textContent = homeScore;
+}
+function addTwoGuest() {
+  guestScore = guestScore + 2;
+  scoreGuest.textContent = guestScore;
+}
+
+// Add three
+
+function addThreeHome() {
+  homeScore = homeScore + 3;
+  scoreHome.textContent = homeScore;
+}
+function addThreeGuest() {
+  guestScore = guestScore + 3;
+  scoreGuest.textContent = guestScore;
+}
+
+// Period
+
+function periodNr() {
+  if (periodNumber < 4) {
+    periodNumber++;
+    period.textContent = periodNumber;
+  } else {
+    periodNumber = 4;
+    period.textContent = periodNumber;
+  }
+}
+
+// Players
+
+function addPlayers() {
+  playerNr++;
+  playerNumber.textContent = playerNr;
+}
+
+function substractPlayers() {
+  if (playerNr > 1) {
+    playerNr--;
+    playerNumber.textContent = playerNr;
+  } else {
+    playerNr = 0;
+    playerNumber.textContent = playerNr;
+  }
+}
+
+// foults
+
+function foultAdd() {
+  foultNr++;
+  foultsNumber.textContent = foultNr;
+}
+
+// new game
+
+function newGame() {
+  homeScore = 0;
+  guestScore = 0;
+  periodNumber = 0;
+  playerNr = 0;
+  foultNr = 0;
+
+  scoreHome.textContent = 0;
+  scoreGuest.textContent = 0;
+  period.textContent = 0;
+  playerNumber.textContent = 0;
+  foultsNumber.textContent = 0;
+}
